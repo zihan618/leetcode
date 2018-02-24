da68c5541c46f9ee6819b4b54ae1b65ba8c2d0ab
zihan618 <1226007238@qq.com>
2018-02-24 14:12:14 +0800

    正则表达式第二种方法 不完全

--- src/leetcode/zhengze.java
+++ src/leetcode/zhengze.java
@@ -8,7 +8,7 @@ public class zhengze {
 
 	public static void main(String[] args) {
 		
-		System.out.println(new zhengze().isMatch("aa", "a*"));
+		System.out.println(new zhengze().isMatch2("aa", ".*"));
 	
 	}
 
@@ -19,6 +19,9 @@ public class zhengze {
 	 * @return
 	 */
 	public boolean isMatch(String s, String p) {
+		if( p.isEmpty()) {
+			return !s.isEmpty();
+		}
 		ArrayList<String> s_subs = new ArrayList<>();
 		s_subs.add("");
 		for( int i=0; i<s.length(); i++) {
@@ -61,11 +64,11 @@ public class zhengze {
 						//matrix[i][j] = (( i==1 && j==0)? true:matrix[i-1][j-1]) && isSimpleMatch(tmp_s, tmp_p);
 					}else {
 						if( j == 0) {
-							matrix[i][j] = matrix[i-1][j] && isSimpleMatch( tmp_s, tmp_p);
+							matrix[i][j] = matrix[i-1][j] && isSimpleMatch( tmp_s, tmp_p);//从上到下
 						}else {
-							matrix[i][j] = (matrix[i-1][j] && isSimpleMatch( tmp_s, tmp_p)) 
-									|| ( (matrix[i][j-1]) && isSimpleMatch("", tmp_p))
-									|| ( (matrix[i-1][j-1]) && isSimpleMatch(tmp_s, tmp_p));
+							matrix[i][j] = (matrix[i-1][j] && isSimpleMatch( tmp_s, tmp_p)) //从左到右
+									|| ( (matrix[i][j-1]) && isSimpleMatch("", tmp_p)) //从左到右 匹配空
+									|| ( (matrix[i-1][j-1]) && isSimpleMatch(tmp_s, tmp_p)); //左上到右下
 						}
 					}
 				}
@@ -94,4 +97,40 @@ public class zhengze {
 		return false;
 	}
 
+	/**
+	 * 跟自己想的大同小异 不过没有吧pattern 分为一段一段 ，也就省略了ArrayList的使用 ，更方便一些   
+	 * @param s
+	 * @param p
+	 * @return
+	 */
+	public boolean isMatch2( String s, String p) {
+		if( p.isEmpty()) {
+			return s.isEmpty();
+		}
+		boolean[][] matrix = new boolean[s.length()+1][p.length()+1];
+		matrix[0][0] = true;
+		//初始化 空的情况
+		for( int i=1; i<p.length()+1; i++) {
+			if(p.charAt(i-1) == '*') {
+				matrix[0][i] = matrix[0][i-2];
+			}
+		}
+		//System.err.println(matrix[0][3]);
+		for( int i=1; i<s.length()+1; i++) {
+			for( int j=1; j<p.length()+1; j++) {
+				if( p.charAt(j-1) == '.') {
+					matrix[i][j] = matrix[i-1][j-1];
+				}else if (p.charAt(j-1) != '*') {
+					matrix[i][j] = matrix[i-1][j-1] && p.charAt(j-1)==s.charAt(i-1);
+				}else {
+					matrix[i][j] = (matrix[i][j-2])//从左到右
+							||(matrix[i-1][j] && p.charAt(j-2)==s.charAt(i-1))//从上到下
+							||(matrix[i-1][j-2] && p.charAt(j-2)==s.charAt(i-1))//左上到右下
+							|| (p.charAt(j-2) == '.' && matrix[i-1][j-2]);//左上到右下 且 是.*
+				}
+			}
+			//System.out.println(matrix[1][1]);
+		}
+		return matrix[s.length()][p.length()];
+	}
 }
