68f11c6df12fb6aa5150620b777298f12bf8f38f
zihan618 <1226007238@qq.com>
2018-03-08 23:50:33 +0800

    最大 十字 解决

--- src/leetcode/LargestPlusSign.java
+++ src/leetcode/LargestPlusSign.java
@@ -5,11 +5,11 @@ import java.util.stream.Stream;
 public class LargestPlusSign {
 
 	public static void main(String[] args) {
-		System.out.println( new LargestPlusSign().orderOfLargestPlusSign(5, new int[][] {{4,2}}));
+		System.out.println( new LargestPlusSign().orderOfLargestPlusSign2(5, new int[][] {{4,2}}));
 	}
 
 	/**
-	 * 暴力解法
+	 * 暴力解法 每个点求出以它为中心的最大十字半径  复杂度 n^3
 	 * 
 	 * @param N
 	 * @param mines
@@ -27,10 +27,69 @@ public class LargestPlusSign {
 				for(int k=0; k<min+1; k++) {
 					if(!matrix[i+k][j] && !matrix[i-k][j] &&!matrix[i][j-k] &&!matrix[i][j+k]) {
 						res = Math.max(res, k+1);
+					}else {
+						break;
 					}
 				}
 			}
 		}
 		return res;
+	}
+	/**
+	 * 比较巧妙的一个方法  除去不可能的情况之后 就是十字的点了
+	 * @param N
+	 * @param mines
+	 * @return
+	 */
+	public int orderOfLargestPlusSign2(int N, int[][] mines) {
+		boolean[][] matrix = new boolean[N][N];
+		int left_pos = N*N;
+		for(int i=0; i<mines.length; i++) {
+			matrix[mines[i][0]][mines[i][1]] = true;
+			left_pos--;
+		}
+		int res = 0;
+		for(int i=1; left_pos!=0; i++) {
+			for(int j=0; j<N; j++) {
+				if(!matrix[i-1][j]) {
+					matrix[i-1][j] = true;
+					left_pos--;
+				}
+				if(!matrix[N-i][j]) {
+					matrix[N-i][j] = true;
+					left_pos--;
+				}
+				if(!matrix[j][i-1]) {
+					matrix[j][i-1] = true;
+					left_pos--;
+				}
+				if(!matrix[j][N-i]) {
+					matrix[j][N-i] = true;
+					left_pos--;
+				}
+			}
+			for(int j=0; j<mines.length; j++) {
+				if(i <= mines[j][0] && !matrix[mines[j][0]-(i)][mines[j][1]]) {
+					matrix[mines[j][0]-(i)][mines[j][1]] = true;
+					left_pos--;
+				}
+				if(N-i-1 >= mines[j][0] && !matrix[mines[j][0]+(i)][mines[j][1]]) {
+					matrix[mines[j][0]+(i)][mines[j][1]] = true;
+					left_pos--;
+				}
+				if(i <= mines[j][1] && !matrix[mines[j][0]][mines[j][1]-(i)]) {
+					matrix[mines[j][0]][mines[j][1]-i] = true;
+					left_pos--;
+				}
+				if(N-i-1 >= mines[j][1] && !matrix[mines[j][0]][mines[j][1]+(i)]) {
+					matrix[mines[j][0]][mines[j][1]+i] = true;
+					left_pos--;
+				}
+			}
+			res = i;
+			
+		}
+		return res;
+		
 	}
 }
